d1ae82dfc243c1d3417ecb6604fa1f7bc02c78ad
Jo Ito <sqrtnull@gmail.com>
2020-06-15 10:31:03 +0900

    changed fastio

--- Utility/fastio.cpp
+++ Utility/fastio.cpp
@@ -1,11 +1,3 @@
 /* <fastio> */
-
-struct FastIO
-{
-  FastIO() {
-    std::cin.tie(0);
-    std::ios_base::sync_with_stdio(0); 
-  }
-} fastio;
-
+struct FIO{FIO(){std::cin.tie(0);std::cout.tie(0);std::ios_base::sync_with_stdio(0);std::cout<<std::fixed<<std::setprecision(15);}}fIO;
 /* <fastio> */

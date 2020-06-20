4a86233ce85c5f87b0435acbe64f511c0683337c
Jo Ito <sqrtnull@gmail.com>
2020-06-20 14:03:59 +0900

    random changes

--- Utility/modint.cpp
+++ Utility/modint.cpp
@@ -21,26 +21,11 @@ struct mint
     bool operator==(const mint& a)const{ return v == a.v; }
     mint pow(lnt k)const{ mint r(1),t(v); while(k){ if(k&1) r*=t; t*=t; k>>=1; } return r; }
     mint inv()const{ return pow(MOD-2); }
-    static mint comb(lnt n, lnt k) {
-        if(n-k<k) k=n-k;
-        mint num(1), dom(1);
-        for(int i=0;i<k;i++) { num*=n-i; dom*=i+1; }
-        return num/dom;
-    }
+    static mint comb(lnt n, lnt k) { if(n-k<k) k=n-k; mint num(1), dom(1); for(int i=0;i<k;i++) { num*=n-i; dom*=i+1; } return num/dom; }
     static std::vector<mint> construct_comb(int n) {
-        std::vector<mint> c(n+1);
-        mint a = 1; c[0] = a;
-        for(int i=1;i<=n;i++) {
-            a = a*mint(n+1-i)/i;
-            c[i] = a;
-        }
-        return c;
+        std::vector<mint> c(n+1); mint a = 1; c[0] = a; for(int i=1;i<=n;i++) { a = a*mint(n+1-i)/i; c[i] = a; } return c;
     }
-    static std::vector<mint> construct_fact(int n) {
-        std::vector<mint> f(n+1,1);
-        for(int i=2;i<=n;i++) f[i]=f[i-1]*i;
-        return f;
-    }
+    static std::vector<mint> construct_fact(int n) { std::vector<mint> f(n+1,1); for(int i=2;i<=n;i++) f[i]=f[i-1]*i; return f; }
 };
 std::istream& operator>>(std::istream&i,mint&a){ lnt t; i>>t; a=mint(t); return i; }
 std::ostream& operator<<(std::ostream&o,const mint&a){ o<<a.v; return o; }
--- Utility/template.cpp
+++ Utility/template.cpp
@@ -28,7 +28,6 @@
 #define print(i) std::cout << (i) << '\n'
 #define all(v) (v).begin(), (v).end()
 using lnt = long long;
-constexpr lnt INF = 2e18;
 struct FIO{FIO(){std::cin.tie(0);std::cout.tie(0);std::ios_base::sync_with_stdio(0);std::cout<<std::fixed<<std::setprecision(15);}}fIO;
 /*-*/
 

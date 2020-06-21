fc560f9aae90e876cb546f291d8026ea76a94887
Jo Ito <sqrtnull@gmail.com>
2020-06-21 15:38:59 +0900

    fixed indent

--- Algorithm/sort_indexes.cpp
+++ Algorithm/sort_indexes.cpp
@@ -2,10 +2,10 @@
 
 template <typename T>
 std::vector<int> sort_indexes(const std::vector<T> &v) {
-  std::vector<int> idx(v.size());
-  std::iota(idx.begin(), idx.end(), 0);
-  std::sort(idx.begin(), idx.end(), [&v](int i1, int i2) {return v[i1] < v[i2];});
-  return idx;
+	std::vector<int> idx(v.size());
+	std::iota(idx.begin(), idx.end(), 0);
+	std::sort(idx.begin(), idx.end(), [&v](int i1, int i2) {return v[i1] < v[i2];});
+	return idx;
 }
 
 /* <sort_indexes> */
